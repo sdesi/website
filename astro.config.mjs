4b35165e08a52ec4cc1be95291a565a5df114682
sdesi <whitewyksa@ya.ru>
2025-12-11 02:07:53 +0300

    17

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   // локальная разработка — base = "/"
-  // потом, если будешь деплоить на GitHub Pages, сюда вернём base/site
+  // потом, еслnp деплоить на GitHub Pages, сюда вернём base/site
   base: "/",
 });
